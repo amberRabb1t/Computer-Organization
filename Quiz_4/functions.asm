ee406c192f1f3caf86f11ce4a4a44559ce558493
amberRabb1t <cathanasd@ece.auth.gr>
2026-02-27 21:05:09 +0200

    Better presentation

--- Quiz_4/functions.asm
+++ Quiz_4/functions.asm
@@ -1,3 +1,4 @@
+partition:
 	addi $sp, $sp, -20
 	sw $ra, 0($sp)
 	sw $s1, 4($sp)
@@ -36,3 +37,23 @@ OUT:
 	lw $s1, 4($sp)
 	lw $ra, 0($sp)
 	addi $sp, $sp, 20
+	
+qsort:
+	slt $t0, $a0, $a1		#if f<l
+	beq $t0, $zero, LEAVE
+	addi $sp, $sp, -16
+	sw $ra, 8($sp)
+	sw $a0, 0($sp)			#f
+	sw $a1, 4($sp)			#l
+	jal partition
+	lw $a0, 0($sp)
+	addi $a1, $v0, -1		#p-1
+	sw $v0, 12($sp)			#p
+	jal qsort
+	lw $t0, 12($sp)
+	addi $a0, $t0, 1		#p+1
+	lw $a1, 4($sp)
+	jal qsort
+	lw $ra, 8($sp)
+	addi $sp, $sp, 16
+LEAVE:
