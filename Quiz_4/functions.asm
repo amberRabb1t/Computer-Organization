758cef880811f7a2e20c8d885c5bdb9d666779ea
amberRabb1t <cathanasd@ece.auth.gr>
2026-02-27 21:08:56 +0200

    Indentation changes

--- Quiz_4/functions.asm
+++ Quiz_4/functions.asm
@@ -11,32 +11,32 @@ partition:
 	move $s2, $a0		#i
 	move $s3, $a0		#j
 	move $s4, $a1		#l
-LOOP:
-	slt $t0, $s3, $s4		#if j<l
-	beq $t0, $zero, OUT
-	sll $t0, $s3, 2
-	add $t0, $s0, $t0
-	lw $t1, 0($t0)		#v[j]
-	slt $t0, $t1, $s1		#if v[j]<pivot
-	beq $t0, $zero, GGGONEXT
-	move $a0, $s2
-	addi $s2, $s2, 1
-	move $a1, $s3
-	jal swap
-GGGONEXT:
-	addi $s3, $s3, 1
-	j LOOP
-OUT:
-	move $a0, $s2
-	move $a1, $s4
-	jal swap
-	move $v0, $s2
-	lw $s4, 16($sp)
-	lw $s3, 12($sp)
-	lw $s2, 8($sp)
-	lw $s1, 4($sp)
-	lw $ra, 0($sp)
-	addi $sp, $sp, 20
+	LOOP:
+		slt $t0, $s3, $s4		#if j<l
+		beq $t0, $zero, OUT
+		sll $t0, $s3, 2
+		add $t0, $s0, $t0
+		lw $t1, 0($t0)		#v[j]
+		slt $t0, $t1, $s1		#if v[j]<pivot
+		beq $t0, $zero, GGGONEXT
+		move $a0, $s2
+		addi $s2, $s2, 1
+		move $a1, $s3
+		jal swap
+	GGGONEXT:
+		addi $s3, $s3, 1
+		j LOOP
+	OUT:
+		move $a0, $s2
+		move $a1, $s4
+		jal swap
+		move $v0, $s2
+		lw $s4, 16($sp)
+		lw $s3, 12($sp)
+		lw $s2, 8($sp)
+		lw $s1, 4($sp)
+		lw $ra, 0($sp)
+		addi $sp, $sp, 20
 	
 qsort:
 	slt $t0, $a0, $a1		#if f<l
@@ -56,4 +56,4 @@ qsort:
 	jal qsort
 	lw $ra, 8($sp)
 	addi $sp, $sp, 16
-LEAVE:
+	LEAVE:
